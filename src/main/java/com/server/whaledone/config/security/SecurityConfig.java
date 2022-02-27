301eadde9a7fb3a79caa643b6cec2d4c86b9a307
bbeomgeun <miki308@naver.com>
2022-02-28 00:55:10 +0900

    Add: SpringSecurity Config 추가

---
+++ src/main/java/com/server/whaledone/config/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.server.whaledone.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**", "/swagger-resources/**",
+                "/swagger-ui/**", "/webjars/**", "/swagger/**","/swagger.json");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsFilter)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/sign-in", "/api/v1/sign-up").permitAll()
+                .antMatchers("/api/v1/membership")
+                .access("hasRole('ROLE_MEMBERSHIP') or hasRole('ROLE_ADMIN')")
+                .anyRequest().authenticated();
+    }
+}
