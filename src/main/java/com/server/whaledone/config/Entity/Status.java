00d71bf856f75f3f535be1758f369922801ad3d3
bbeomgeun <miki308@naver.com>
2022-03-01 02:19:21 +0900

    Add: 유저 역할의 Role.java, 상태를 관리하는 Status.java ENUM 추가

---
+++ src/main/java/com/server/whaledone/config/Entity/Status.java
@@ -0,0 +1,7 @@
+package com.server.whaledone.config.Entity;
+
+public enum Status {
+    ACTIVE, // 활성화
+    DORMANT, // 휴면
+    DELETED, // 탈퇴
+}
---
+++ src/main/java/com/server/whaledone/user/entity/Role.java
@@ -0,0 +1,7 @@
+package com.server.whaledone.user.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_MEMBERSHIP,
+    ROLE_ADMIN
+}
