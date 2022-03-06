24710cabfb7e9fe8d2c6e420e8731725fa93492e
bbeomgeun <miki308@naver.com>
2022-03-06 18:43:50 +0900

    Add: ExceptionStatus, valid ExceptionHandler 추가

--- src/main/java/com/server/whaledone/config/response/exception/CustomExceptionStatus.java
+++ src/main/java/com/server/whaledone/config/response/exception/CustomExceptionStatus.java
@@ -7,10 +7,31 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CustomExceptionStatus {
 
-    INVALID_INPUT_VALUE(true, 400, "유효하지 않은 입력입니다.");
+    // Common
+    INVALID_INPUT_VALUE(true, "C001", "유효하지 않은 입력입니다."),
+
+    /**
+     * User
+     */
+    // Email
+    USER_EXISTS_EMAIL(true,"UE001","이미 가입된 이메일이에요."),
+    USER_INVALID_EMAIL(true, "UE002", "올바른 이메일 주소가 아니에요"),
+    USER_NOT_EXISTS_EMAIL(true, "UE003", "존재하는 이메일이 없어요."),
+
+    // Nickname
+    USER_EXISTS_NICKNAME(true, "UN001", "이미 가입된 닉네임이에요."),
+    USER_INVALID_NICKNAME(true, "UN002", "한글/영문/숫자/특수문자(*,._-+!?)까지 입력 가능해요."),
+
+    // Password
+    USER_INVALID_PASSWORD(true, "UP001", "영문 대문자/소문자/숫자 3가지를 포함해서 입력하세요."),
+    USER_NOT_MATCHES_PASSWORD(true, "UP002", "일치하지 않는 비밀번호입니다."),
+
+    // InvitationCode
+    INVITATION_EXPIRED_CODE(true, "I001", "만료된 초대코드입니다."),
+    INVITATION_INVALID_CODE(true, "I002", "일치하는 초대코드가 없습니다");
 
     private final boolean responseStatus;
-    private final int code;
+    private final String code;
     private final String message;
 
 
--- src/main/java/com/server/whaledone/config/response/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/server/whaledone/config/response/exception/ExceptionControllerAdvice.java
@@ -4,6 +4,7 @@ import com.server.whaledone.config.response.ResponseService;
 import com.server.whaledone.config.response.result.CommonResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,8 +16,14 @@ public class ExceptionControllerAdvice {
     private final ResponseService responseService;
 
     @ExceptionHandler(CustomException.class)
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResult customException(CustomException customException) {
         return responseService.getFailResult(customException.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CommonResult validationException(MethodArgumentNotValidException exception) {
+        return responseService.getFailResult(CustomExceptionStatus.INVALID_INPUT_VALUE);
     }
 }
