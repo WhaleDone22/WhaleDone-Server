2a866d721d0dc7fa6fd964b776e67c5c958254f1
bbeomgeun <miki308@naver.com>
2022-03-10 14:41:24 +0900

    Chore: response 출력

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -22,6 +22,7 @@ for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
   echo "> #${RETRY_COUNT} trying..."
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT})
+  echo "> Response = ${RESPONSE_CODE}"
 
   if [ ${RESPONSE_CODE} -eq 200 ]; then
     echo "> New WAS successfully running"
