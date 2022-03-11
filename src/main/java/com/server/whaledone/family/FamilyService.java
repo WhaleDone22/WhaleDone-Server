ac1bfde781e012cfa60ff4ab028e9c9c469f9077
bbeomgeun <miki308@naver.com>
2022-03-11 17:56:17 +0900

    Fix: user에 familyId가 업데이트되지 않는 문제 해결 (dirty checking)

--- src/main/java/com/server/whaledone/family/FamilyService.java
+++ src/main/java/com/server/whaledone/family/FamilyService.java
@@ -1,8 +1,12 @@
 package com.server.whaledone.family;
 
+import com.server.whaledone.config.response.exception.CustomException;
+import com.server.whaledone.config.response.exception.CustomExceptionStatus;
 import com.server.whaledone.config.security.auth.CustomUserDetails;
 import com.server.whaledone.family.dto.response.CreateFamilyResponseDto;
 import com.server.whaledone.family.entity.Family;
+import com.server.whaledone.user.UserRepository;
+import com.server.whaledone.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +18,7 @@ import java.util.Date;
 public class FamilyService {
 
     private final FamilyRepository familyRepository;
+    private final UserRepository userRepository;
 
     private long CODE_EXPIRATION_TIME = 48 * 60 * 60 * 1000L; // 1초 * 60(초) * 60(분) * 48(시간) = 48시간
 
@@ -28,9 +33,12 @@ public class FamilyService {
         Family newFamily = Family.builder()
                 .invitationCode(invitationCode)
                 .build();
-        familyRepository.save(newFamily);
 
-        newFamily.addMember(userDetails.getUser());
+        User user = userRepository.findByEmailAndStatus(userDetails.getEmail(), userDetails.getStatus())
+                .orElseThrow(() -> new CustomException(CustomExceptionStatus.USER_NOT_EXISTS));
+
+        newFamily.addMember(user);
+        familyRepository.save(newFamily);
 
         return CreateFamilyResponseDto.builder()
                 .invitationCode(invitationCode)
