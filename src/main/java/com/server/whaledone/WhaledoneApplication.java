e7ffe59c74c7fc3140fc3c2de996d131c2c470e7
bbeomgeun <miki308@naver.com>
2022-03-30 21:11:26 +0900

    Chore: aws key 시스템 변수로 변경

--- src/main/java/com/server/whaledone/WhaledoneApplication.java
+++ src/main/java/com/server/whaledone/WhaledoneApplication.java
@@ -1,21 +1,15 @@
 package com.server.whaledone;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class WhaledoneApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "classpath:aws.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(WhaledoneApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		SpringApplication.run(WhaledoneApplication.class, args);
 	}
 
 }
