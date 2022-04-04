7f2a0d43b2705c3dbf92425bc99c7c30d4ba558f
bbeomgeun <miki308@naver.com>
2022-04-05 02:47:34 +0900

    Feat: 예외에 로그 기능 추가

--- src/main/java/com/server/whaledone/config/response/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/server/whaledone/config/response/exception/ExceptionControllerAdvice.java
@@ -3,9 +3,11 @@ package com.server.whaledone.config.response.exception;
 import com.server.whaledone.config.response.ResponseService;
 import com.server.whaledone.config.response.result.CommonResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.access.AuthorizationServiceException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
+@Slf4j
 public class ExceptionControllerAdvice {
 
     private final ResponseService responseService;
@@ -20,18 +23,36 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public CommonResult commonException(Exception exception) {
+        log.error(String.format("InternalServerErrorException : %s", exception.getMessage()), exception);
         return responseService.getFailResult(CustomExceptionStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(CustomException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResult customException(CustomException customException) {
+        log.info(String.format("Business Exception : %s", customException.getStatus().getMessage()), customException);
         return responseService.getFailResult(customException.getStatus());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResult validationException(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+
+        String validMessage = builder.toString();
+
+        log.info(String.format("MethodArgumentNotValidException %s", validMessage), exception);
         return responseService.getFailResult(CustomExceptionStatus.INVALID_INPUT_VALUE);
     }
 
