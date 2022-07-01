993e1e622b6fb63a99a31695a3998870cd173d63
bbeomgeun <miki308@naver.com>
2022-07-01 21:45:27 +0900

    Fix: jwt secret key 숨기기

--- src/main/java/com/server/whaledone/config/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/server/whaledone/config/security/jwt/JwtTokenProvider.java
@@ -5,25 +5,33 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.server.whaledone.config.security.auth.CustomUserDetailsService;
 import com.server.whaledone.user.entity.RoleType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private long EXPIRATION_TIME = 1000L * 60 * 60 * 10 * 24 * 10; // 10일 (1/1000초)
-    private String SECRET_KEY = "temp";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+    private long EXPIRATION_TIME = 100 * 24 * 60 * 60 * 1000L;
     private String HEADER_STRING = "X-AUTH-TOKEN";
     private String TOKEN_PREFIX = "Bearer ";
 
     private final CustomUserDetailsService customUserDetailsService;
 
+    @PostConstruct
+    private void init() {
+        SECRET_KEY = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes());
+    }
 
     // jwt 토큰 생성
     public String createToken(String email, RoleType roleType) {
